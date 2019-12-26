4397dab9d896033e02b124ae59a57b3696d9da1a
Thomas Humphrey <thumphrey9123@gmail.com>
2019-12-26 16:47:04 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,6 @@
 first_number = 12
 second_number = 3
 sum = first_number + second_number
-def calculator (first_number, second_number)
-  sum = first_number + second_number
-  return sum
-end
+difference = first_number - second_number
+
 
